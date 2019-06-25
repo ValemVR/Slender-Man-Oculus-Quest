af03fd7352b4d44d2615b5135b43a74b8f24e25a
Valem <qvalem.vr@gmail.com>
2019-06-25 18:03:29 +0200

    Switch to the good one

---
+++ Slender Man VR/Assets/Page.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page : MonoBehaviour
+{
+    static int numberofpages = 0;
+    public GameObject win;
+    public AudioSource pagesound;
+
+    private void Start()
+    {
+        numberofpages = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pagesound.Play();
+        numberofpages++;
+
+        if(numberofpages == 5)
+        {
+            win.SetActive(true);
+        }
+
+        Destroy(gameObject);
+    }
+}
